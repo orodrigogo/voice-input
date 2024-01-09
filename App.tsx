6fb438cb9a0ece1fcea43fdbe418d84684bdbb3e
rodrigo <rodrigo.rgtic@gmail.com>
2024-01-09 14:16:10 -0300

    add voice

--- App.tsx
+++ App.tsx
@@ -1,17 +1,58 @@
+import { useEffect, useState } from "react"
+import { Pressable, StyleSheet, TextInput, View, LogBox } from "react-native"
 import { StatusBar } from "expo-status-bar"
 import { Feather } from "@expo/vector-icons"
-import { Pressable, StyleSheet, TextInput, View } from "react-native"
+
+import Voice, { SpeechResultsEvent } from "@react-native-voice/voice"
+
+LogBox.ignoreLogs(["new NativeEventEmitter"])
 
 export default function App() {
+  const [search, setSearch] = useState("")
+  const [isListening, setIsListening] = useState(false)
+
+  function onSpeechResults({ value }: SpeechResultsEvent) {
+    const text = value ?? []
+    setSearch(text.join().replace(",", " "))
+  }
+
+  async function handleListening() {
+    try {
+      if (isListening) {
+        await Voice.stop()
+        setIsListening(false)
+      } else {
+        setSearch("")
+        await Voice.start("pt-BR")
+        setIsListening(true)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    Voice.onSpeechResults = onSpeechResults
+  }, [])
+
   return (
     <View style={styles.container}>
       <StatusBar style="dark" backgroundColor="transparent" translucent />
 
       <View style={styles.header}>
-        <TextInput style={styles.input} placeholder="Pesquisar..." />
+        <TextInput
+          style={styles.input}
+          placeholder={isListening ? "Gravando..." : "Pesquisar... "}
+          onChangeText={setSearch}
+          value={search}
+        />
 
-        <Pressable style={styles.button}>
-          <Feather name="mic" color="#FFF" size={24} />
+        <Pressable style={styles.button} onPress={handleListening}>
+          <Feather
+            name={isListening ? "pause" : "mic"}
+            color="#FFF"
+            size={24}
+          />
         </Pressable>
       </View>
     </View>
